9703088b576fc271d18bd916b8572f853f8632c8
Sudhanshu kumar <sudhanshukumar.jnvr@gmail.com>
2023-03-30 01:47:14 +0530

    doctor login

---
+++ src/pages/Doctor/DoctorLogin.jsx
@@ -0,0 +1,127 @@
+import { useState } from "react";
+import { authentication } from "../../firebase";
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
+import axios from "axios";
+import { json } from "body-parser";
+
+function DoctorLogin() {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [expandForm, setExpandForm] = useState(false);
+  const [OTP, setOTP] = useState('');
+  const navigate=useNavigate();
+  const gotoUserPage=(e)=>{
+    let doctorNumber=phoneNumber.slice(-10);
+    axios.get(`http://localhost:8081/doctor/doctor-by-contact/${doctorNumber}`)
+    .then((response)=>{
+         console.log(response.data);
+         //localStorage.setItem('doctor', JSON.stringify(response.data));
+         navigate('/PatientPage',{
+            state:{patientNum:doctorNumber}
+          })
+    })
+    .catch((error)=>{
+        console.error('error on fatching doctor object',error);
+    })
+  }
+  const generateRecaptcha = () => {
+    window.recaptchaVerifier = new RecaptchaVerifier(
+      "recaptcha-container",
+      {
+        size: "invisible",
+        callback: (response) => {
+          // reCAPTCHA solved, allow signInWithPhoneNumber.
+          console.log("captcha reviceved");
+        },
+      },
+      authentication
+    );
+  };
+
+  const requestOTP = (e) => {
+    e.preventDefault();
+    if (phoneNumber.length >= 12) {
+      setExpandForm(true);
+      generateRecaptcha();
+      let appVerifier = window.recaptchaVerifier;
+      signInWithPhoneNumber(authentication, phoneNumber, appVerifier)
+        .then((confirmationResult) => {
+          window.confirmationResult = confirmationResult;
+          console.log("here");
+        })
+        .catch((error) => {
+          console.log('not getting otp');
+        });
+    }
+  };
+
+  const verifyOTP = (e) => {
+    let otp = e.target.value;
+    setOTP(otp);
+    if (otp.length === 6) {
+      // console.log(otp);
+      let confirmationResult = window.confirmationResult;
+      confirmationResult
+        .confirm(otp)
+        .then((result) => {
+          // User signed in successfully.
+          const user = result.user;
+          console.log(user);
+          gotoUserPage();
+          // ...
+        })
+        .catch((error) => {
+          // User couldn't sign in (bad verification code?)
+          // ...
+        });
+    }
+  };
+ console.log(OTP);
+  return (
+    <>
+     <div >
+         <div className="col-md-2 offset-md-8 box" >
+          <h3>Enter Your number</h3>
+          <div className="mb-2">
+            <PhoneInput
+              defaultCountry="IN"
+              international
+              withCountryCallingCode
+              value={phoneNumber}
+              onChange={setPhoneNumber}
+            />
+          </div>
+
+          {expandForm === true ? (
+            <>
+              <div className="mp-3">
+                <h6>OTP</h6>
+                <input
+                  type="number"
+                  className="form-control"
+                  id="otpInput"
+                  value={OTP}
+                  onChange={verifyOTP}
+                />
+                <div>
+                  Please enter the one time pin
+                </div>
+                <button type="submit" className="btn btn-primary" onClick={verifyOTP}>submit</button>
+              </div>
+            </>
+          ) : null}
+          {expandForm === false ? (
+            <button type="submit" className="btn btn-primary" onClick={gotoUserPage}>
+              Request Otp
+            </button>
+          ) : null}
+          <div id="recaptcha-container"></div>
+        </div>
+        </div>
+    </>
+  );
+}
+
+export default DoctorLogin;
